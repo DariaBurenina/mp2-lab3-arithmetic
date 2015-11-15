7e939e64af467aa9601b71e12869bea903fbd56b
DariaBurenina <daria.burenina@gmail.com>
2015-11-15 19:36:40 +0300

    Разбиение на буквы, знаки и проверка скобок

--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -5,24 +5,38 @@
 TEST(Arithmetic, can_do_partition)
 {
 	char variables[] = "qwertyuiopasdfghjklzxcvbnm";
+	char signs[] = "()+*-/";
 	char temp[] = "a+b*c-7+t";
 	char type1[256];
-	int k = 0;
+	char type2[256];
+	int k1 = 0;
+	int k2 = 0;
 	for( int i = 0; i < strlen(temp); i++)
 		for( int j = 0; j < strlen(variables); j++)
 			if( temp[i] == variables[j] )
 			{
-				type1[k] = temp[i];
-				k++;
+				type1[k1] = temp[i];
+				k1++;
 			}
+	for( int i = 0; i < strlen(temp); i++)
+		for( int j = 0; j < strlen(signs); j++)
+		{
+			if( temp[i] == signs[j] )
+			{
+				type2[k2] = temp[i];
+				k2++;
+			}
+		}
 	ASSERT_NO_THROW(Partition (temp));
 	EXPECT_EQ(type1[0], 'a');
 	EXPECT_EQ(type1[1], 'b');
 	EXPECT_EQ(type1[2], 'c');
 	EXPECT_EQ(type1[3], 't');
+	EXPECT_EQ(type2[0], '+');
+	EXPECT_EQ(type2[1], '*');
+	EXPECT_EQ(type2[2], '-');
+	EXPECT_EQ(type2[3], '+');
 }
-
-
 
 
 TEST(Arithmetic, can_check_the_brackets)
