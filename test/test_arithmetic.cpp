4cc9df5c3eeb1a1b48fa77f8d8d2a1ab269e3298
DariaBurenina <daria.burenina@gmail.com>
2015-11-21 12:54:29 +0300

    Добавлена проверка операций и операндов

--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -2,40 +2,88 @@
 #include "arithmetic.h"
 
 
-TEST(Arithmetic, can_do_partition_)
+TEST(Arithmetic, can_do_partition_into_variables)
 {
 	char temp[] = "a+b*c-7+t";
-	ASSERT_NO_THROW(Partition (temp));
+	ASSERT_NO_THROW(Partition_into_variables (temp));
+}
+
+TEST(Arithmetic, can_do_partition_into_signs)
+{
+	char temp[] = "a+b*c-7+t";
+	ASSERT_NO_THROW(Partition_into_signs (temp));
+}
+
+TEST(Arithmetic, can_do_partition_into_figures)
+{
+	char temp[] = "a+b*c-7+t";
+	ASSERT_NO_THROW(Partition_into_figures (temp));
 }
 
 
 TEST(Arithmetic, can_check_the_brackets)
 {
-	char temp1[] = "(5+4)*9";
-	ASSERT_NO_THROW( Check_the_brackets(temp1) );
+	char temp[] = "(5+4)*9";
+	ASSERT_NO_THROW( Check_the_brackets(temp) );
 }
 
 TEST(Arithmetic, return_true_if_brackets_are_entered_correctly)
 {
-	char temp1[] = "(5+4)*9";
-	EXPECT_EQ(true, Check_the_brackets(temp1));
+	char temp[] = "(5+4)*9";
+	EXPECT_EQ(true, Check_the_brackets(temp));
 }
 
 TEST(Arithmetic, return_false_if_brackets_are_entered_incorrectly_closing_brackets_are_more)
 {
-	char temp2[] = "(5+4)*9)";
-	EXPECT_EQ(false, Check_the_brackets(temp2));
+	char temp[] = "(5+4)*9)";
+	EXPECT_EQ(false, Check_the_brackets(temp));
 }
 
 TEST(Arithmetic, return_false_if_brackets_are_entered_incorrectly_opening_brackets_are_more)
 {
-	char temp3[] = "(5+4)*9(";
-	EXPECT_EQ(false, Check_the_brackets(temp3));
+	char temp[] = "(5+4)*9(";
+	EXPECT_EQ(false, Check_the_brackets(temp));
 }
 
 TEST(Arithmetic, return_false_if_brackets_are_entered_incorrectly_the_first_bracket_is_closing)
 {
 
-	char temp4[] = ")(5+4)*9(";
-	EXPECT_EQ(false, Check_the_brackets(temp4));
+	char temp[] = ")(5+4)*9(";
+	EXPECT_EQ(false, Check_the_brackets(temp));
+}
+
+TEST(Arithmetic, can_check_of_operatoins_and_operands)
+{
+	char temp[] = "(5+4)*9";
+	ASSERT_NO_THROW( Check_of_operatoins_and_operands(temp) );
+}
+
+TEST(Arithmetic, return_false_if_two_operands_in_a_row)
+{
+	char temp[] = "(5+4)*9*ab";
+	EXPECT_EQ(false, Check_of_operatoins_and_operands(temp));
+}
+
+TEST(Arithmetic, return_false_if_two_signs_in_a_row)
+{
+	char temp[] = "(5+4)*9*a+*";
+	EXPECT_EQ(false, Check_of_operatoins_and_operands(temp));
+}
+
+TEST(Arithmetic, return_false_if_at_the_end_of_expression_is_an_sign_after_operand)
+{
+	char temp[] = "(5+4)*9*a+";
+	EXPECT_EQ(false, Check_of_operatoins_and_operands(temp));
+}
+
+TEST(Arithmetic, return_false_if_at_the_end_of_expression_is_an_sign_after_figure)
+{
+	char temp[] = "(5+4)*9*1+";
+	EXPECT_EQ(false, Check_of_operatoins_and_operands(temp));
+}
+
+TEST(Arithmetic, can_check_of_operatoins_and_operandsdqf)
+{
+	char temp[] = "(5+4)*9*a+1";
+	EXPECT_EQ(true, Check_of_operatoins_and_operands(temp));
 }
